e0a148e4d3583017d9b6c5d789f57cfe0a07c4b1
danny <a7527744@gmail.com>
2022-08-01 22:45:16 +0800

    make it better

--- src/App.js
+++ src/App.js
@@ -3,6 +3,9 @@ import './App.css';
 import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Home from './components/pages/Home';
+import Services from './components/pages/Service';
+import Arts from './components/pages/Arts';
+import SignUp from './components/pages/SignUp';
 
 function App() {
   return (
@@ -11,6 +14,9 @@ function App() {
         <Navbar/>
         <Routes>
           <Route path="/" element={<Home/>}/>
+          <Route path='/services' element={<Services/>}/>
+          <Route path='/arts' element={<Arts/>}/>
+          <Route path='/sign-up' element={<SignUp/>}/>
           
         </Routes>
       </Router>
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -47,7 +47,7 @@ function Navbar() {
                 </Link>
               </li>
               <li className='nav-item'>
-                <Link to="/home" className='nav-links' onClick={closeMobileMenu}>
+                <Link to="/" className='nav-links' onClick={closeMobileMenu}>
                   Home
                 </Link>
               </li>
