2912227b19110a755d9001421999dc42a896f0ae
dannylin <a7527744@gmail.com>
2022-09-02 11:26:48 +0800

    add :作者介紹

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -3,12 +3,14 @@ import '../../App.css';
 import Section from '../Section';
 import Footer from '../Footer';
 import Introduction from '../Introduction';
+import Artist from '../Artist';
 
 function Home({lang}){
     return(
         <>
             <Section lang={lang}/>
             <Introduction/>
+            <Artist/>
             <Footer lang={lang}/>
             
         </>
