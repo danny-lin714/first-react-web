9cf2540ca517cddda9b1bf8fdf46b209f809638f
danny <a7527744@gmail.com>
2022-09-09 17:23:52 +0800

    :add: fetch api(登入功能)

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -5,7 +5,25 @@ import '../App.css'
 
 function Login({lang,event}) {
 
-
+    const add_user=()=>{
+        const body={
+            user_name: {username},
+            user_account: {account},
+            user_password: {password}
+        }
+        fetch("http://localhost:5000/user",{
+            method: "POST",
+            body:JSON.stringify(body)
+        }).then(() => {
+            alert("登入成功!");
+            navigate("/home");
+          })
+          .catch((e) => {
+            if (e.response.error) {
+              alert("帳號或密碼錯誤！");
+            }
+          });
+    }
     return (
         <div className='container'>
             <h1>LOGIN</h1>
@@ -13,7 +31,7 @@ function Login({lang,event}) {
                 <form action='/'>
                     <TextField placeholder='Account'/>
                     <TextField placeholder='Password'/>
-                    <div><button></button></div>
+                    <div><button onClick={add_user}></button></div>
                 </form>
             </div>
 
