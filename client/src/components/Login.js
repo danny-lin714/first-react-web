8a0df33f0c5e9b6cdd4df3990fd944579c5fe412
danny <a7527744@gmail.com>
2022-09-11 22:04:37 +0800

    delete: fetch

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -10,25 +10,7 @@ function Login({lang,event}) {
 
 
 
-    const add_user=()=>{
-        const body={
-            user_name: {username},
-            user_account: {account},
-            user_password: {password}
-        }
-        fetch("http://localhost:5000/user",{
-            method: "POST",
-            body:JSON.stringify(body)
-        }).then(() => {
-            alert("登入成功!");
-            navigate("/home");
-          })
-          .catch((e) => {
-            if (e.response.error) {
-              alert("帳號或密碼錯誤！");
-            }
-          });
-    }
+    
     return (
         <div className='container'>
             <img src="images/img-5.jpg" alt=""/>
