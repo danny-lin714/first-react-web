8d58de05a0229f5c513833bbd13718cd7e2062b8
danny <a7527744@gmail.com>
2022-09-17 17:03:54 +0800

    feature: 登入功能

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -7,7 +7,29 @@ function Login({lang,event}) {
 
     const [inputValue_acc, setInputValue_acc] = useState('')
     const [inputValue_pwd, setInputValue_pwd] = useState('')
-
+    
+    const add_user=()=>{
+        fetch("http://localhost:5000/user",{
+            method: "POST",
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify({
+                "user_name":inputValue_user,
+                "user_account": inputValue_acc,
+                "user_password": inputValue_pwd
+            })
+        })
+        .then(response => response.json())
+        .then(json => console.log(json));
+    }
+    const set_user=(e)=>{
+        setInputValue_user(e.target.value)
+    }
+    const set_acc=(e)=>{
+        setInputValue_acc(e.target.value)
+    }
+    const set_pwd=(e)=>{
+        setInputValue_pwd(e.target.value)
+    }
 
 
     
@@ -19,11 +41,9 @@ function Login({lang,event}) {
                 <form action='/'>
 
                     <TextField placeholder='Account'
-                               value={inputValue_acc}
-                               onChange={(e) => setInputValue_acc(e.target.value)}/>
+                               setValue={set_acc}/>
                     <TextField type='password' placeholder='Password'
-                               value={inputValue_pwd}
-                               onChange={(e) => setInputValue_pwd(e.target.value)}/>
+                               setValue={set_pwd}/>
                     <div><button></button></div>
 
                 </form>
--- server/index.js
+++ server/index.js
@@ -13,6 +13,38 @@ app.get("/api",(req,res)=>{
     res.json({"users":["userone","asd"]})
 })
 
+app.post("/login",jsonParser,(req,res)=>{
+    var user_account = req.body.user_account
+    var user_password = req.body.user_password
+    
+    db.query('SELECT * FROM user',function(error,result){
+        var t=0
+        if (error) throw error;
+        for(var i in result){
+            if(user_account==result[i].user_account){
+                if(user_password==result[i].user_password){
+                    t=1
+                    return res.status(200).send("hello"+" "+result[i].user_name)
+                }
+            }
+        }
+        if(t==0){
+            return res.status(400).send("login again")
+        }
+    })
+    
+})
+/*
+-------------------------------------------------------------------------------------------------------
+使用者操作
+*/
+app.get("/user",(req,res)=>{
+    db.query('SELECT * FROM user',function(error,result){
+        console.log(result)
+        if (error) throw error;
+        return res.send({data:result})
+        })
+    });
 app.post("/user",jsonParser,(req, res)=>{
     console.log(req.body)
     var user_name = req.body.user_name
@@ -32,10 +64,9 @@ app.post("/user",jsonParser,(req, res)=>{
     }
     
 
-    
-    /*
-    
-    */
 });
+/*
+-------------------------------------------------------------------------------------------------
+*/
 
 app.listen(5000)
